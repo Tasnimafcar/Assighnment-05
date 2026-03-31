1406a99a9ea438572314df47906045f6352f1c57
Tasnimafcar <tasnimafsar123@gmail.com>
2026-03-31 06:06:39 +0600

    secound commit

--- script.js
+++ script.js
@@ -26,4 +26,32 @@ const allbtn = document.getElementById("allbtn");
 const openbtn = document.getElementById("openbtn");
 const closedbtn = document.getElementById("closedbtn");
 
+if (allbtn) {
+    const btns = [allbtn, openbtn, closedbtn];
 
+    function setActive(selectedbtn) {
+        btns.forEach(btn => {
+            btn.classList.remove("bg-[#4A00FF]", "text-white");
+            btn.classList.add("bg-white", "border", "border-gray-300", "text-gray-700")
+        });
+        selectedbtn.classList.add("bg-[#4A00FF]", "text-white");
+        selectedbtn.classList.remove("bg-white", "border", "border-gray-300", "text-gray-700")
+    };
+
+    allbtn.addEventListener("click", () => setActive(allbtn));
+    openbtn.addEventListener("click", () => setActive(openbtn));
+    closedbtn.addEventListener("click", () => setActive(closedbtn));
+};
+
+// Issus Count 
+const issueContainer = document.getElementById("issueContainer");
+const issusCount = document.getElementById("issusCount");
+
+if(issueContainer){
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((data) => {
+        const issus = data.data;
+        issusCount.innerText = issus.length;
+    });
+};
